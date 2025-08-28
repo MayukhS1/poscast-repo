6d83000b3eede7e026cbe46169ce63c6d1d93b2d
Mayukh Sasmal <49561618+MayukhS1@users.noreply.github.com>
2025-08-28 16:38:57 +0000

    xmlns tag fixed

--- feed.py
+++ feed.py
@@ -7,8 +7,8 @@ with open('feed.yaml', 'r') as file:
     
     #rss data
     rss_element = xml_tree.Element('rss', {'version': '2.0' ,
-        'xmins: itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
-        'xmns: content': 'http://purl.org/rss/1.0/modules/content/'})
+        'xmlns:itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+        'xmlns:content': 'http://purl.org/rss/1.0/modules/content/'})
 
 # channel
 channel_element = xml_tree.SubElement(rss_element, 'channel')
